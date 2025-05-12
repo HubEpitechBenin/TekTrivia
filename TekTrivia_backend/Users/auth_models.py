7bb65d06bffec01d4aac1c542576d65c8e47b42f
KiriHakai <ronnel.dassi@epitech.eu>
2025-05-12 14:18:04 +0100

    feat: add models for Custom Token creation

--- TekTrivia_backend/Users/auth_models.py
+++ TekTrivia_backend/Users/auth_models.py
@@ -4,42 +4,46 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+from Users.models import Player, Admin
+
+
 #Function to generate a custom UUID
 def generate_uuid():
     return uuid.uuid4()
 
+def generate_key():
+    return binascii.hexlify(os.urandom(20)).decode()
+
 class Token(models.Model):
     public_id = models.UUIDField(db_index=True, unique=True, default=generate_uuid)
-    key = models.CharField(max_length=40)
+    access = models.CharField(max_length=40)
     refresh = models.CharField(max_length=40, null=True)
 
     def save(self, *args, **kwargs):
-        if not self.key:
-            self.key = self.generate_key()
-            self.refresh = self.generate_key()
+        if not self.access:
+            self.access = generate_key()
+        if not self.refresh:
+            self.refresh = generate_key()
         return super().save(*args, **kwargs)
 
-    def generate_key(self):
-        return binascii.hexlify(os.urandom(20)).decode()
-
     def regenerate(self, force_refresh=False):
-        if settings.TESTING:
-            return #???
-        self.key = self.generate_key()
+        # if settings.TESTING:
+        #     return #???
+        self.access = generate_key()
         if force_refresh:
-            self.refresh = self.generate_key()
+            self.refresh = generate_key()
         self.save()
 
     def __str__(self):
-        return self.key
+        return self.refresh
 
     class Meta:
         abstract = True
 
 class PlayerAuthToken(Token):
-    user = models.OneToOneField('models.Player', on_delete=models.CASCADE)
+    user = models.OneToOneField(Player, on_delete=models.CASCADE)
 
 class AdminAuthToken(Token):
-    user = models.OneToOneField('models.Admin', on_delete=models.CASCADE)
-
+    user = models.OneToOneField(Admin, on_delete=models.CASCADE)
+#
 
