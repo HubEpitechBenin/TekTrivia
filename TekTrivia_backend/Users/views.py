3cf93c8c86e3caab0df6faca854cec746c9112f7
KiriHakai <ronnel.dassi@epitech.eu>
2025-05-12 12:21:16 +0100

    feat: add login views and registration Viewsets for Users

--- TekTrivia_backend/Users/views.py
+++ TekTrivia_backend/Users/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import viewsets, views, status, response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from Users.models import Player, Admin
+from Users.serializers import PlayerRegistrationSerializer, AdminRegistrationSerializer
+
 
 # Create your views here.
+
+class LoginTestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return response.Response(
+            {
+              "message": f"Hello World! Hello, {request.user.username}!"
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    model = Player
+
+    def create(self, request, **kwargs):
+        serializer = PlayerRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(
+            {
+                "message": "Player registered successfully!"
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+class AdminViewSet(viewsets.ModelViewSet):
+    model = Admin
+
+    def create(self, request, **kwargs):
+        serializer = AdminRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(
+            {
+                "message": "Admin registered successfully!"
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+
