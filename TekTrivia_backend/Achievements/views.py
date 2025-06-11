2e27d4b93359c562da1d146aa001ac40822d49ae
KiriHakai <ronnel.dassi@epitech.eu>
2025-06-11 23:16:37 +0100

    feat(achievements): add TitleViewSet and fix typo in model
    Introduced `TitleViewSet` to manage title-related endpoints with authentication. Corrected typo in `Achievements.models` by renaming `nex_rank` to `next_rank`.

--- TekTrivia_backend/Achievements/views.py
+++ TekTrivia_backend/Achievements/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from Achievements.models import Title
+from Achievements.serializers import TitleSerializer
+
 
 # Create your views here.
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    serializer_class = TitleSerializer
+    # FIXME: Make sure authentication is correctly set.
+    permission_classes = [IsAuthenticated]
+    authentication_classes = []
+    queryset = Title.objects.all()
+
+
