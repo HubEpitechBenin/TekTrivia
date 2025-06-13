6ddc1528966ab9ff2dc8d9adfcd2c6150f0a9bc5
Aimane01 <aimane.alassane@epitech.eu>
2025-06-13 14:18:39 +0200

    [feat]: adding views for quiz models

--- TekTrivia_backend/Quizzes/views.py
+++ TekTrivia_backend/Quizzes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import QuizType, Type, Difficulty, Category, Resource, Quiz, Question, QuizBody
+from .serializers import CategorySerializer, ResourceSerializer, QuizSerializer, QuestionSerializer, QuizBodySerializer
+
+class QuizBodyInfoAPIView(APIView):
+    def get(self, request):
+        quizbodies = QuizBody.objects.all()
+        serializer = QuizBodySerializer({
+            'quizzes': quizbodies,
+            'count': len(quizbodies) 
+        })
+        return Response(serializer.data)
 
 # Create your views here.
