79b97e84f19501721fea204e57f4c15d3c5c5c97
KiriHakai <ronnel.dassi@epitech.eu>
2025-06-17 06:57:41 +0100

    feat(urls): add endpoint for simple quiz  ai generation

--- TekTrivia_backend/SimpleQuiz/urls.py
+++ TekTrivia_backend/SimpleQuiz/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QuizAPIView, QuestionAPIView, AnswerAPIView, CategoryAPIView
+from .views import QuizAPIView, QuestionAPIView, AnswerAPIView, CategoryAPIView, QuizzGenerationView
 
 urlpatterns = [
     path('quizzes/', QuizAPIView.as_view(), name='quiz-list-create'),
     path('questions/', QuestionAPIView.as_view(), name='question-create'),
     path('answers/', AnswerAPIView.as_view(), name='answer-create'),
-    path('categories/', CategoryAPIView.as_view(), name='category-create')
+    path('categories/', CategoryAPIView.as_view(), name='category-create'),
+    path('generate/', QuizzGenerationView.as_view(), name='quiz-generate'),
 ]
